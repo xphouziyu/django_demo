3f556c76775e4e5546e10c81069da6dfc20bb860
ye <xphouziyu@qq.com>
2019-07-31 03:13:22 +0800

    写好用户认证提交，错误

--- s1/ss1/views.py
+++ s1/ss1/views.py
@@ -2,10 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.shortcuts import HttpResponse, render
+from django.shortcuts import HttpResponse, render, redirect
 
 def Home(request):
     return HttpResponse("<h1>你好</h1>")
 
 def login(reques):
-    return render(reques, "login.html")
+    if reques.method == "POST":
+        name = reques.POST.get('user')
+        pwd = reques.POST.get('pwd')
+        if name == "alex" and pwd == "123":
+            return redirect("http://www.baidu.com")
+        else:
+            return render(reques, "login.html", {"error_msg": "错误"})
+    elif reques.method == "GET":
+        return render(reques, "login.html")
